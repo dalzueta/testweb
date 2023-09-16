5c8be7b85c8ec46b1f309369cdfa7e9ae282bb25
daniel alzueta <daniel.alzueta@astropay.com>
2023-09-16 00:43:05 -0300

    live ascii art

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const asciiText = document.querySelector('.ascii-text');
+window.addEventListener('load', function () {
+    // Coloca aquí el código que deseas ejecutar cuando todos los recursos de la página, incluyendo imágenes y hojas de estilo, se hayan cargado completamente.
 
-let rotation = 0;
+    const asciiText = document.querySelector('.ascii-text');
+    const toggleSoundButton = document.getElementById('toggleSoundButton');
+    const audio = document.getElementById('myAudio');
+    
+    let rotation = 0;
+    let soundEnabled = false;
 
-function rotateText() {
-    rotation += 5;
-    asciiText.style.transform = `rotate(${rotation}deg)`;
-    requestAnimationFrame(rotateText);
-}
+    function rotateText() {
 
-rotateText();
+        rotation += 5;
+        audio.play();
+        asciiText.style.transform = `rotate(${rotation}deg)`;
+        requestAnimationFrame(rotateText);
+    }
+
+    rotateText();
+    
+    toggleSoundButton.addEventListener('click', () => {
+        if (soundEnabled) {
+            // audio.pause(); // Pausa el sonido
+            // toggleSoundButton.textContent = 'hace click';
+        } else {
+            audio.play(); // Reproduce el sonido
+            
+            toggleSoundButton.textContent = 'Alla le estan Clickeando !!! ';
+            this.setTimeout(()=> {
+                toggleSoundButton.style.display = 'none';  
+            }, 2500)
+            // toggleSoundButton.style.display = 'none'; 
+        }
+        soundEnabled = !soundEnabled; // Alterna el estado del sonido
+    });
+
+    
+});
