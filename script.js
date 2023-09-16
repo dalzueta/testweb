2dc2e555f499f0de5efd7b760953198403992099
daniel alzueta <daniel.alzueta@astropay.com>
2023-09-15 23:40:00 -0300

    live ascii art

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const asciiText = document.querySelector('.ascii-text');
 let rotation = 0;
 
 function rotateText() {
-    rotation += 10;
+    rotation += 5;
     asciiText.style.transform = `rotate(${rotation}deg)`;
     requestAnimationFrame(rotateText);
 }
